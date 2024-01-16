a701a31c7f2a4009fdada3d3d07df73833cdfac7
Guilherme Melo <limaguilherme.melo@gmail.com>
2024-01-15 21:26:33 -0300

    salário

--- algoritmo-java/java-poo/salario/src/application/Program.java
+++ algoritmo-java/java-poo/salario/src/application/Program.java
@@ -5,12 +5,6 @@ import java.util.Scanner;
 
 public class Program {
 
-    /*
-    * Fazer um programa para ler os dados de um funcionário (nome, salário bruto e imposto). 
-    * Em seguida, mostrar os dados do funcionário(nome e salário líquido). Em seguida, aumentar 
-    * o salário do funcionário com base em uma porcentagem dada 
-    * (somente o salário bruto é afetado pela porcentagem) e mostrar novamente os dados do funcionário.
-    */
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
